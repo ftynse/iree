6ce8df0416e91ea67822c948d52ec66c2a049244
Alex Zinenko <zinenko@google.com>
2022-05-11 10:31:54 +0000

    make sandbox smoke test pass

--- llvm-external-projects/iree-dialects/include/iree-dialects-c/Dialects.h
+++ llvm-external-projects/iree-dialects/include/iree-dialects-c/Dialects.h
@@ -33,6 +33,15 @@ MLIR_DECLARE_CAPI_DIALECT_REGISTRATION(IREELinalgExt, iree_linalg_ext);
 
 MLIR_DECLARE_CAPI_DIALECT_REGISTRATION(LinalgTransform, iree_linalg_transform);
 
+//===--------------------------------------------------------------------===//
+// TransformDialect
+//===--------------------------------------------------------------------===//
+
+MLIR_DECLARE_CAPI_DIALECT_REGISTRATION(Transform, transform);
+
+MLIR_CAPI_EXPORTED void
+ireeRegisterTransformDialectExtensions(MlirContext context);
+
 //===----------------------------------------------------------------------===//
 // IREEPyDMDialect
 //===----------------------------------------------------------------------===//
